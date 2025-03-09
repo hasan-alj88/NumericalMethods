68b429490b118ee77be2e0a234e23a8938bf7b2c
hhj <hasan.alj88@yahoo.com>
2025-03-09 14:31:32 +0300

    Simplify root-finding example by removing unused content
    Removed excess code and markdown cells from the root-finding example notebook. This reduces clutter and focuses solely on the main content of the example, improving readability and maintainability.

--- FindRoots/OpenMethods/ThreePointSecantMethod.py
+++ FindRoots/OpenMethods/ThreePointSecantMethod.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass, field
 
+import numpy as np
+
 from FindRoots.RootFinder import RootFinder
 from StopConditions.StopIfEqual import StopIfZero
 from StopConditions.StopIfNaN import StopIfNaN
@@ -25,7 +27,7 @@ class ThreePointSecantMethod(RootFinder):
     def df(self, t) -> float:
         t0 = t - self.dt
         t1 = t + self.dt
-        return (self.function(t1) - self.function(t0)) / (t1 - t0)
+        return (self.function(t1) - self.function(t0)) / (2*self.dt)
 
     def d2f(self, t) -> float:
         t0 = t - self.dt
@@ -43,24 +45,74 @@ class ThreePointSecantMethod(RootFinder):
             f=self.function(self.t0),
             df_dt=self.df(self.t0),
             d2f_dt2=self.d2f(self.t0),
+            det =self.df(self.t0) * self.d2f(self.t0),
+            log='Initial State'
         )
 
     def step(self) -> dict:
-        """
-        t_{n+1} = t_n - \frac{f(t_n)}{f'(t_n)} \left( 1 + \frac{1}{2} \frac{f(t_n) f''(t_n)}{(f'(t_n))^2} \right).
-        """
         t_n = self.history['t']
         f_n = self.history['f']
 
         df_dt = self.df(t_n)
         d2f_dt2 = self.d2f(t_n)
 
-        t_np1 = t_n - (f_n / df_dt) * (1 + 0.5 * f_n * d2f_dt2 / df_dt ** 2)
-        f_np1 = self.function(t_np1)
+        det = df_dt ** 2 - 2 * f_n * d2f_dt2
+
+        # if det > 0:
+        #     t_np1_1 = t_n - (df_dt-np.sqrt(det)) / d2f_dt2
+        #     t_np1_2 = t_n - (df_dt+np.sqrt(det)) / d2f_dt2
+        #     f_np1_1 = self.function(t_np1_1)
+        #     f_np1_2 = self.function(t_np1_2)
+        #     if abs(f_np1_1) < abs(f_np1_2):
+        #         t_np1 = t_np1_1
+        #         f_np1 = f_np1_1
+        #         log = f'det > 0, t_np1_1 is better'
+        #     else:
+        #         t_np1 = t_np1_2
+        #         f_np1 = f_np1_2
+        #         log = f'det > 0, t_np1_2 is better'
+        # elif det < 0:
+        #     t_np1 = t_n - f_n/ df_dt
+        #     f_np1 = self.function(t_np1)
+        #     log = f'det < 0, using newton-raphson'
+        # else:
+        #     t_np1 = t_n - df_dt / d2f_dt2
+        #     f_np1 = self.function(t_np1)
+        #     log = f'det = 0'
+
+        t_np1_1 = np.real(t_n - (df_dt-np.sqrt(det)) / d2f_dt2)
+        t_np1_2 = np.real(t_n - (df_dt+np.sqrt(det)) / d2f_dt2)
+        f_np1_1 = self.function(t_np1_1)
+        f_np1_2 = self.function(t_np1_2)
+        if abs(f_np1_1) < abs(f_np1_2):
+            t_np1 = t_np1_1
+            f_np1 = f_np1_1
+            log = f'det > 0, t_np1_1 is better'
+        else:
+            t_np1 = t_np1_2
+            f_np1 = f_np1_2
+            log = f'det > 0, t_np1_2 is better'
+
 
         return dict(
             t=t_np1,
             f=f_np1,
             df_dt=df_dt,
             d2f_dt2=d2f_dt2,
+            det=det,
+            log=log,
         )
+
+if __name__ == '__main__':
+    import numpy as np
+    import pandas as pd
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    solver = ThreePointSecantMethod(
+        function=lambda t: -np.tanh(t**2-9),
+        t0=0,
+        dt=0.01,
+        max_iterations=100
+    )
+    df = solver.run()
+    print(df)
